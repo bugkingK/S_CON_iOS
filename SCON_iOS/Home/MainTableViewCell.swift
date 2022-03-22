6c9ddd31358dfb5854fb93ee54605e3f999dcc1f
kimjiin <kj980926@naver.com>
2022-03-22 16:00:40 +0900

    [#2] Feat: MainScreen 파트별 구성

---
+++ SCON_iOS/Home/MainTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MainTableViewCell.swift
+//  SCON_iOS
+//
+//  Created by 김지인 on 2022/03/22.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SCON_iOS/Home/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  SCON_iOS
+//
+//  Created by 김지인 on 2022/03/19.
+//
+
+import UIKit
+
+struct ContestName{
+    let name: String
+    let systemImg: String
+    let imgColor: UIColor
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var mainTableView: UITableView!
+    let mainList = [ContestName(name: "IT 부문", systemImg: "tv.circle.fill", imgColor: .blue),
+                    ContestName(name: "미디어콘텐츠 부문", systemImg: "headphones.circle.fill", imgColor: .orange),
+                    ContestName(name: "SW 경진대회", systemImg: "wave.3.right.circle.fill", imgColor: .green)]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainTableView.delegate = self
+        mainTableView.dataSource = self
+        mainTableView.isScrollEnabled = false //스크롤 금지
+    }
+
+}
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mainList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "mainCell", for: indexPath) as? MainTableViewCell else { return UITableViewCell() }
+        let data = mainList[indexPath.row]
+        cell.titleLabel.text = data.name
+        //이미지 설정
+        let image = UIImage(systemName: data.systemImg)?.withTintColor(data.imgColor, renderingMode: .alwaysOriginal)
+        cell.imgView.image = image
+        
+        return cell
+    }
+    
+}
+
+
+extension ViewController: UITableViewDelegate{
+    
+}
