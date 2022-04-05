b2b219e2ded8a9d47b65f8fe5d78fd11b444be95
kimjiin <kj980926@naver.com>
2022-04-05 14:28:47 +0900

    [#11] Design: 버튼 Layout 수정

---
+++ SCON_iOS/Login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  SCON_iOS
+//
+//  Created by 김지인 on 2022/04/05.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+
+}
